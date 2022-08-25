7e7d55b00feedda4f9f283750f09ad8f0f2382dc
Hairun Huang <hairunhuang@gmail.com>
2022-08-24 21:18:40 -0700

    get films

--- simple_api_request.py
+++ simple_api_request.py
@@ -4,3 +4,7 @@ req = requests.get("https://swapi.dev/api/people/2/")
 person = req.json()
 print(f"name is {person['name']}")
 print(f"birth year is {person['birth_year']}")
+
+for film in person['films']:
+    film = requests.get(film).json()
+    print("film is: ", film['title'])
