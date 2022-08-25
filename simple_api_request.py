c5364dca027abd4c1458a4a344b29a33f0c830f6
Hairun Huang <hairunhuang@gmail.com>
2022-08-24 21:14:54 -0700

    swapi

--- simple_api_request.py
+++ simple_api_request.py
@@ -1,4 +1,6 @@
 import requests
 
-req = requests.get("https://hairun.netlify.app")
-print(req.status_code)
+req = requests.get("https://swapi.dev/api/people/2/")
+person = req.json()
+print(f"name is {person['name']}")
+print(f"birth year is {person['birth_year']}")
