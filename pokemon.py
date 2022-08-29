5cda756fe178eeb2adfeaa43aeb13948d464a8b2
Hairun Huang <hairunhuang@gmail.com>
2022-08-29 14:41:06 -0700

    poke api

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+pokemon = input("choose a pokemon: ")
+pokemon = pokemon.lower()
+
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+req = requests.get(url)
+
+data = req.json()
+print(f"weight is : {data['weight']}")
