1b1d63696572daade7a27cb0141ccc22a79e42d0
Hairun Huang <hairunhuang@gmail.com>
2022-08-29 15:37:51 -0700

    overwriting

--- first_class.py
+++ first_class.py
@@ -14,6 +14,13 @@ class Tiger(Animal):
     def speak(self):
         print('meow')
 
+class HouseCat(Animal):
+    fur_color = 'black'
+    def speak(self):
+        print("hellow")
 
 tiger = Tiger()
 tiger.speak()
+cat = HouseCat()
+cat.speak()
+print(cat.fur_color)
