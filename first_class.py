04a1796769d7bf4c4998f02cb24cb79ab848f042
Hairun Huang <hairunhuang@gmail.com>
2022-08-29 15:42:56 -0700

    if you want to use this method, you have to implement it. (interface)

--- first_class.py
+++ first_class.py
@@ -2,25 +2,16 @@ class Animal:
     fur_color = 'orange'
 
     def speak(self):
-        print('raaaawr')
-
+        raise NotImplementedError
     def eat(self):
         pass
 
     def chase(self):
         pass
 
-class Tiger(Animal):
+class HouseCat(Animal):
     def speak(self):
         print('meow')
 
-class HouseCat(Animal):
-    fur_color = 'black'
-    def speak(self):
-        print("hellow")
-
-tiger = Tiger()
-tiger.speak()
 cat = HouseCat()
 cat.speak()
-print(cat.fur_color)
