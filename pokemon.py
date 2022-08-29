426774946baa95b4fc1ac9abb2d8a7d874583f0f
Hairun Huang <hairunhuang@gmail.com>
2022-08-29 14:53:50 -0700

    poke api

--- pokemon.py
+++ pokemon.py
@@ -7,9 +7,9 @@ pokemon = pokemon.lower()
 url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
 req = requests.get(url)
 
-data = req.json()
-print(f"weight is : {data['weight']}")
-print("abilities: ")
-
-for ability in data['abilities']:
-    print(ability['ability']['name'])
+if req.status_code == 200:
+    data = req.json()
+    for ability in data['abilities']:
+        print(ability['ability']['name'])
+else:
+    print("pokemon not found")
