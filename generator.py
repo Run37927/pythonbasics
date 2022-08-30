8df4ff582b35d6b63e920faee3926dc339c549c1
Hairun Huang <hairunhuang@gmail.com>
2022-08-30 00:14:47 -0700

    geni() has been exhausted, so big num cannot print

--- generator.py
+++ generator.py
@@ -1,9 +1,11 @@
-a_list = [1, 2, 3, 4, 5, 6]
-
-
 def geni():
     for num in range(50):
         yield num**num
 
-for big_num in geni():
+
+my_var_generator = geni()
+all_nums = list(my_var_generator)
+print(all_nums)
+
+for big_num in my_var_generator:
     print(big_num)
