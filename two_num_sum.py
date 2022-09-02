ef018a52c953b42e514bb137fd29e2738979098a
Hairun Huang <hairunhuang@gmail.com>
2022-09-01 18:51:49 -0700

    time: nlogn, space: 1

--- two_num_sum.py
+++ two_num_sum.py
@@ -1,11 +1,24 @@
 def twoNumberSum(array, targetSum):
     # Write your code here.
-    for i in range(len(array) -1):
-        first_num = array[i]
-        for j in range(i+1, len(array)):
-            second_num = array[j]
-            if first_num + second_num == targetSum:
-                return [first_num, second_num]
+    # for i in range(len(array) -1):
+    #     first_num = array[i]
+    #     for j in range(i+1, len(array)):
+    #         second_num = array[j]
+    #         if first_num + second_num == targetSum:
+    #             return [first_num, second_num]
+    # return []
+
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        current_sum = array[left] + array[right]
+        if current_sum == targetSum:
+            return [array[left], array[right]]
+        elif current_sum < targetSum:
+            left += 1
+        elif current_sum > targetSum:
+            right -= 1
     return []
 
 
