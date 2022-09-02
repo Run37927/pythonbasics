5c6c57960880fc56c592fb537b4e3691f44ccb0a
Hairun Huang <hairunhuang@gmail.com>
2022-09-01 21:29:48 -0700

    time: O(n) space: O(1) no storing anything other than two pointers

--- is_valid_subsequence.py
+++ is_valid_subsequence.py
@@ -1,8 +1,18 @@
 def is_valid_subsequence(array, sequence):
-    arr_index = 0
+    # arr_index = 0
+    # seq_index = 0
+    # while arr_index < len(array) and seq_index < len(sequence):
+    #     if array[arr_index] == sequence[seq_index]:
+    #         seq_index += 1
+    #     arr_index += 1
+    # return seq_index == len(sequence)
+
+
+
     seq_index = 0
-    while arr_index < len(array) and seq_index < len(sequence):
-        if array[arr_index] == sequence[seq_index]:
+    for num in array:
+        if seq_index == len(sequence):
+            break
+        if sequence[seq_index] == num:
             seq_index += 1
-        arr_index += 1
-    return seq_index == len(sequence)
+    return seq_index ==  len(sequence)
