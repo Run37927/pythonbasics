a466d1575524b23727a92fa195108ebbafc1eb90
Hairun Huang <hairunhuang@gmail.com>
2022-09-04 12:22:26 -0700

    range(len(queries)) no need to minus 1 as as one multiplies zero

--- min_waittime.py
+++ min_waittime.py
@@ -3,12 +3,18 @@ def minimumWaitingTime(queries):
     if len(queries) == 1:
         return 0
     queries.sort() # built in sort nlogn
-    res = []
-    t = 0
-    for i in range(len(queries) - 1):
-        t += queries[i]
-        res.append(t)
-    return sum(res)
+    # res = []
+    # t = 0
+    # for i in range(len(queries) - 1):
+    #     t += queries[i]
+    #     res.append(t)
+    # return sum(res)
+
+    min_time = 0
+    for i in range(len(queries)):
+        min_time += queries[i] * (len(queries) - (i + 1))
+    return min_time
+
 
 
 q = [3, 2, 1, 2, 6]
