423c1445054e2dd92b85fe5d10847960f40feb04
Hairun Huang <hairunhuang@gmail.com>
2022-09-04 11:56:44 -0700

    time: n space: n

---
+++ min_waittime.py
@@ -0,0 +1,15 @@
+def minimumWaitingTime(queries):
+    # Write your code here.
+    if len(queries) == 1:
+        return 0
+    queries.sort()
+    res = []
+    t = 0
+    for i in range(len(queries) - 1):
+        t += queries[i]
+        res.append(t)
+    return sum(res)
+
+
+q = [3, 2, 1, 2, 6]
+print(minimumWaitingTime(q))
