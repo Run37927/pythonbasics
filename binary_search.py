bf3c45ce9aacb606645f2441a5b77b4431ada6fd
Hairun Huang <hairunhuang@gmail.com>
2022-09-05 11:17:32 -0700

    iterative approach

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,32 @@
+# def binary_search(array, target):
+#     # prerequiste: the list is already sorted
+#     return helper(array, target, 0, len(array) - 1)
+#
+#
+# def helper(array, target, left, right):
+#     if left > right:
+#         return -1
+#     middle = (left + right) // 2
+#     if array[middle] == target:
+#         return middle
+#     elif target < array[middle]:
+#         return helper(array, target, left, middle - 1)
+#     else:
+#         return helper(array, target, middle + 1, right)
+#
+
 def binary_search(array, target):
-    # prerequiste: the list is already sorted
     return helper(array, target, 0, len(array) - 1)
 
 
 def helper(array, target, left, right):
-    if left > right:
-        return -1
-    middle = (left + right) // 2
-    if array[middle] == target:
-        return middle
-    elif target < array[middle]:
-        return helper(array, target, left, middle - 1)
-    else:
-        return helper(array, target, middle + 1, right)
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == target:
+            return middle
+        elif target < array[middle]:
+            return helper(array, target, left, middle - 1)
+        else:
+            return helper(array, target, middle + 1, right)
 
+    return -1
