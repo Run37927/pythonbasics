8232ec2a697097a68bfb323e41e2afda6aca112a
Hairun Huang <hairunhuang@gmail.com>
2022-09-05 10:41:38 -0700

    time: n space: constant bc not storing anything

--- get_nth_fib.py
+++ get_nth_fib.py
@@ -1,15 +1,27 @@
-cache = {}
-def get_nth_fib(n):
-    # using memoization
-    global cache
-    if n in cache:
-        return cache[n]
+# cache = {}
+# def get_nth_fib(n):
+#     # using memoization
+#     global cache
+#     if n in cache:
+#         return cache[n]
+#
+#     if n == 0:
+#         result = 0
+#     elif n == 1:
+#         result = 1
+#     else:
+#         result = get_nth_fib(n-1) + get_nth_fib(n-2)
+#     cache[n] = result
+#     return result
 
-    if n == 0:
-        result = 0
-    elif n == 1:
-        result = 1
-    else:
-        result = get_nth_fib(n-1) + get_nth_fib(n-2)
-    cache[n] = result
-    return result
+
+def get_nth_fib(n):
+    # iterative approach
+    last_two = [0,1]
+    counter = 3
+    while counter <= 3:
+        next_fib = last_two[0] + last_two[1]
+        last_two[0] = last_two[1]
+        last_two[1] = next_fib
+        counter += 1
+    return last_two[1] if n > 1 else 0
