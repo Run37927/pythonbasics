9c127966865ae7c4142d9dbc5509bc71f4cf062b
Hairun Huang <hairunhuang@gmail.com>
2022-09-06 14:41:20 -0700

    time: n^2 space: constant

---
+++ selection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(array):
+    current_index = 0  # this is the first index of the current unsorted list
+    while current_index < len(array) - 1:
+        # when current index reach to the last index in the list, that means we are odne
+        smallest_index = current_index  # assign current index to smallest index
+        for i in range(current_index + 1, len(array)):
+            if array[smallest_index] > array[i]:
+                smallest_index = i
+        swap(current_index, smallest_index, array)
+        current_index += 1
+    return array
+
+
+def swap(x, y, a_list):
+    a_list[x], a_list[y] = a_list[y], a_list[x]
