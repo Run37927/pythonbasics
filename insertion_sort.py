a4c46bca5617e6b8abccdca16373ef97aabc9707
Hairun Huang <hairunhuang@gmail.com>
2022-09-05 21:33:08 -0700

    time: n^2 space: constant

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion(array):
+    # looping through array once
+    for i in range(1, len(array)):
+        j = i
+        # looping backwards to do swapping in the tentatively sorted sublist
+        while j > 0 and array[j] < array[j-1]:
+            swap(j, j-1, array)
+            j -= 1
+    return array
+
+
+def swap(k, m, a_list):
+    a_list[k], a_list[m] = a_list[m], a_list[k]
