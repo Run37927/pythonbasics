d063baed284e0df3629945cbcc710c6af737c712
Hairun Huang <hairunhuang@gmail.com>
2022-09-07 23:53:56 -0700

    using tail recursion. space: n, time :n

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,9 +1,15 @@
-def ispalin(string):
-    start = 0
-    end = len(string) - 1
-    while start < end:
-        if string[start] != string[end]:
-            return False
-        start += 1
-        end -= 1
-    return True
+# def ispalin(string):
+#     start = 0
+#     end = len(string) - 1
+#     while start < end:
+#         if string[start] != string[end]:
+#             return False
+#         start += 1
+#         end -= 1
+#     return True
+
+
+def is_palindrome(string, i=0):
+    j = len(string) - 1 - i
+    return True if i >= j else string[i] == string[j] and is_palindrome(string, i+1)
+
