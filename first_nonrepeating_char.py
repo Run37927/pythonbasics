7b1d7d000a3931a373ecdd0107e0b1531d66aa54
Hairun Huang <hairunhuang@gmail.com>
2022-09-08 14:04:41 -0700

    using dictionary.

--- first_nonrepeating_char.py
+++ first_nonrepeating_char.py
@@ -1,9 +1,23 @@
-def find_firstnonrepeatingchar(string):
-    for i in range(len(string)):
-        found_duplicate = False
-        for j in range(len(string)):
-            if string[i] == string[j] and i != j:
-                found_duplicate = True
-        if not found_duplicate:
-            return i
-    return -1
+# def find_firstnonrepeatingchar(string):
+#     for i in range(len(string)):
+#         found_duplicate = False
+#         for j in range(len(string)):
+#             if string[i] == string[j] and i != j:
+#                 found_duplicate = True
+#         if not found_duplicate:
+#             return i
+#     return -1
+
+
+def find(string):
+    # using dictionary
+    counts = {}
+    for char in string:
+        if char not in counts:
+            counts[char] = 1
+        else:
+            counts[char] += 1
+
+    for char in string:
+        if counts[char] == 1:
+            return string.index(char)
