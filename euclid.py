bbfa0c0eec260735398c738383396f42042b66ac
Hairun Huang <hairunhuang@gmail.com>
2022-09-08 16:21:53 -0700

    find greatest common denominator

--- euclid.py
+++ euclid.py
@@ -1,11 +1,17 @@
 def find_gcd(a, b):
-    r = a % b
-    while r != 0:
-        a = b
-        b = r
-        r = a % b
-    return b
+    # r = a % b
+    # while r != 0:
+    #     a = b
+    #     b = r
+    #     r = a % b
+    # return b
 
+    while b != 0:
+        temp = a
+        a = b
+        b = temp % b
+
+    return a
 
 
 print(find_gcd(20, 8))
