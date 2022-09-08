927b65dd0eff2f7e285c6dd2efe30c84797e09da
Hairun Huang <hairunhuang@gmail.com>
2022-09-08 16:38:27 -0700

    easier way to write bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,27 @@
+# def bubble(array):
+#     is_sorted = False
+#     while not is_sorted:
+#         is_sorted = True
+#         for i in range(len(array) - 1):
+#             if array[i] > array[i + 1]:
+#                 swap(i, i + 1, array)
+#                 is_sorted = False
+#     return array
+#
+#
+# def swap(n, k, the_array):
+#     temp = the_array[n]
+#     the_array[n] = the_array[k]
+#     the_array[k] = temp
+
+
 def bubble(array):
-    is_sorted = False
-    while not is_sorted:
-        is_sorted = True
-        for i in range(len(array) - 1):
-            if array[i] > array[i + 1]:
-                swap(i, i + 1, array)
-                is_sorted = False
-    return array
-
-
-def swap(n, k, the_array):
-    temp = the_array[n]
-    the_array[n] = the_array[k]
-    the_array[k] = temp
+    for i in range(len(array) - 1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
 
 
 a_list = [8, 5, 2, 9, 5, 6, 3]
